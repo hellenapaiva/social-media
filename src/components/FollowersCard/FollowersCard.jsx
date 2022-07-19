3ebcefb2d9d798fdcf8881cb8495ff48bac3f5d2
Helena Oliveira <helena.oliveira@hypeflame.com.br>
2022-07-19 16:32:45 -0300

    created followersCard component

---
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./FollowersCard.css";
+
+import { Followers } from "../../Data/FollowersData";
+const FollowersCard = () => {
+  return (
+    <div className="FollowersCard">
+      <h3>Quem seguiu você</h3>
+
+      {Followers.map((follower, id) => {
+        return (
+          <div className="follower">
+            <div>
+              <img src={follower.avatar} alt="" className="followerImg" />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className="button fc-button">Seguir</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FollowersCard;
